d8a06065f3f3e116356344671da5c67fadc12f67
Pasha <zzzkelrath@yandex.ru>
2024-09-11 18:12:19 +0700

    Added ls in task2.sh for debug

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,9 +1,10 @@
+ls data
 tar -xf ../data/archive-part1.tar
 unzip ../data/archive-part2.zip
 a=$(find . -name foo.txt)
 cat $a
 a=$(find . -name bar.txt)
-cat $a
+dcat $a
 a=$(find . -name baz.txt)
 cat $a
 rm -r some
