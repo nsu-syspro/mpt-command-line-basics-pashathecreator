26c25ac90dd51d99c1edbf3462ed05cc3ff4ed7e
Pasha <zzzkelrath@yandex.ru>
2024-09-11 18:07:54 +0700

    Fixed 4

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,5 +1,5 @@
-tar -xf data/archive-part1.tar
-unzip data/archive-part2.zip
+tar -xf ../data/archive-part1.tar
+unzip ../data/archive-part2.zip
 a=$(find . -name foo.txt)
 cat $a
 a=$(find . -name bar.txt)
